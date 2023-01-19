701f7bca894d5df959cea1c1b58fa159d6d1149a
Kevin Harris <hugokharris@gmail.com>
2023-01-19 13:00:37 -0700

    add createSurface() function for main sketch surface

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function createSurface(columns) {
-    
+// assign DOM elements
+const sketchpad = document.querySelector('.sketchpad');
+
+function createSurface(columns, backgroundColor) {
+    sketchpad.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    sketchpad.style.gridTemplateRows = `repeat(${columns}, 1fr)`;
+    sketchpad.style.backgroundColor = backgroundColor;
+
+    // create item divs
+
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < columns; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add(`col${j}`);
+            newDiv.classList.add(`row${i}`);
+            sketchpad.appendChild(newDiv);
+        }
+    }
 }
+
+createSurface(16, 'coral');
