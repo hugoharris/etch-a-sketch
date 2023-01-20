f6217c74b688d995f834c8d15cd0de3d006be304
Kevin Harris <hugokharris@gmail.com>
2023-01-19 17:48:39 -0700

    add basic sketch functionality with spacebar toggle

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // assign DOM elements
 const sketchpad = document.querySelector('.sketchpad');
 
+// set variables
+let pen = false;
+let columns = 64;
+let sketchpadSize = 576;
 
 // create surface function
 function createSurface(columns, backgroundColor) {
@@ -8,36 +12,56 @@ function createSurface(columns, backgroundColor) {
     sketchpad.style.gridTemplateRows = `repeat(${columns}, 1fr)`;
     sketchpad.style.backgroundColor = backgroundColor;
 
-    // create item divs
+    // create item divs and attach row and column class
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < columns; j++) {
             const newDiv = document.createElement('div');
-            newDiv.classList.add(`col${j}`);
-            newDiv.classList.add(`row${i}`);
+            newDiv.classList.add(`col${j}row${i}`);
+            // newDiv.classList.add(`row${i}`);
             sketchpad.appendChild(newDiv);
         }
     }
 }
 
-// temporary exitSketch
-function exitSketch() {
-    return;
-}
+// toggle pen
+function togglePen(e) {
+    console.log(pen);
+    console.log(e.code);
+    if (e.code === "Space") {
+        pen ? pen = false : pen = true;
+    }
+} 
 
-// sketch function
-function sketch(e) {
-    sketchpad.addEventListener('mouseup', exitSketch);
-    
-    // compute current margins of sketchpad to establish offsets for mouse position
-    const sketchpadStyle = window.getComputedStyle(sketchpad);
-    const offsetX = Number(sketchpadStyle.marginLeft.slice(0, -2));
-    const offsetY = Number(sketchpadStyle.marginTop.slice(0, -2));
-    
-    console.log(e.clientX - offsetX, e.clientY - offsetY);
+// mouse movement
+function moveMouse(e) {
+    // is the pen active?
+    if (pen) {
+        // compute current margins of sketchpad to establish offsets for mouse position
+        const sketchpadStyle = window.getComputedStyle(sketchpad);
+        const offsetX = Number(sketchpadStyle.marginLeft.slice(0, -2));
+        const offsetY = Number(sketchpadStyle.marginTop.slice(0, -2));
+
+        // compute cell location
+        const pixelsPerColumn = sketchpadSize / columns;
+        const xPosition = e.clientX - offsetX;
+        const yPosition = e.clientY - offsetY;
+        console.log(xPosition, yPosition, sketchpadSize / columns);
+        const column = columns - Math.floor((sketchpadSize - xPosition) / pixelsPerColumn) - 1;
+        const row = columns - Math.floor((sketchpadSize - yPosition) / pixelsPerColumn) - 1;
+        console.log(column, row);
+
+        // color cell
+        const gridBox = document.querySelector(`.col${column}row${row}`)
+        gridBox.style.backgroundColor = 'black';
+
+        // temporary log of mouse position
+        console.log(xPosition, yPosition); 
+    }
 }
 
 // create surface
-createSurface(16, 'coral');
+createSurface(columns, 'coral');
 
-// if user has mouse down fire first event listenser
-sketchpad.addEventListener('mousedown', sketch);
+// add event listeners
+sketchpad.addEventListener('mousemove', moveMouse);
+document.addEventListener('keypress', togglePen);
